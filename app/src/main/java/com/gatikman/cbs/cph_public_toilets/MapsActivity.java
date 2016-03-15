ff982d2802e744eda1838b2ea0074392ac75cd7f
kristianhg <kristian.h.gundersen@gmail.com>
2016-03-15 16:54:19 +0100

    added bla bla to MapsActivity

--- app/src/main/java/com/gatikman/cbs/cph_public_toilets/MapsActivity.java
+++ app/src/main/java/com/gatikman/cbs/cph_public_toilets/MapsActivity.java
@@ -13,6 +13,7 @@ import com.google.android.gms.maps.model.MarkerOptions;
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+    //Bla bla
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
